597843a0f11c561a6b7f7d98044319d6acbdbcc0
ethylsch <dobriydobryak2012@gmail.com>
2026-05-19 22:45:24 +0300

    commit in work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
+/** @format */
+
 import React, { useCallback, useMemo, useState, useEffect } from "react";
-import { v4 as uuidv4 } from 'uuid';
-import './styles/App.css';
+import { v4 as uuidv4 } from "uuid";
+import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
@@ -13,14 +15,14 @@ import { usePosts } from "./hooks/usePosts";
 import axios from "axios";
 
 const sortOptions = [
-  { value: 'title', name: 'По названию' },
-  { value: 'body', name: 'По описанию' },
+  { value: "title", name: "По названию" },
+  { value: "body", name: "По описанию" },
 ];
 
 function App() {
   const [posts, setPosts] = useState([]);
 
-  const [filter, setFilter] = useState({ sort: '', query: '' });
+  const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
@@ -29,36 +31,43 @@ function App() {
   }, []);
 
   async function fetchPosts() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts",
+    );
     setPosts(response.data);
   }
 
   const removePost = useCallback((id) => {
-    setPosts(prev => prev.filter(p => p.id !== id));
-  }, [])
+    setPosts((prev) => prev.filter((p) => p.id !== id));
+  }, []);
 
   const handleCreatePost = useCallback((newPost) => {
-    setPosts(prev => [...prev, newPost]);
+    setPosts((prev) => [...prev, newPost]);
   }, []);
 
   return (
     <div className="App">
-      <MyButton className="open-modal-btn" onClick={() => setModal(true)}>Если тебе грустно, нажми</MyButton>
+      <MyButton className="open-modal-btn" onClick={() => setModal(true)}>
+        Если тебе грустно, нажми
+      </MyButton>
       <MyModal visible={modal} setVisible={setModal}>
         <h1 className="post__list__title">Ты хуесос</h1>
       </MyModal>
-      <PostForm
-        create={handleCreatePost}
-      />
+      <PostForm create={handleCreatePost} />
       <hr className="separator" />
       <PostFilter
         sortOptions={sortOptions}
         filter={filter}
         setFilter={setFilter}
       />
-      <PostList className="post-delete-btn" remove={removePost} title='Список какой-то хуйни' posts={sortedAndSearchedPosts} />
+      <PostList
+        className="post-delete-btn"
+        remove={removePost}
+        title="Список какой-то хуйни"
+        posts={sortedAndSearchedPosts}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
