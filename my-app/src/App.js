a2279aaa05aff1e610ea0a69f675c80fb49b6f4b
ethylsch <dobriydobryak2012@gmail.com>
2026-05-20 03:41:01 +0300

    pagination

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,6 +13,11 @@ import MyModal from "./components/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePosts";
 import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
+import { getPageCount } from "./utils/pages";
+import { getPagesArray } from "./utils/pages";
 
 const sortOptions = [
   { value: "title", name: "По названию" },
@@ -21,21 +26,25 @@ const sortOptions = [
 
 function App() {
   const [posts, setPosts] = useState([]);
-
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState();
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  let pagesArray = getPagesArray(totalPages);
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers["x-total-count"];
+    setTotalPages(getPageCount(totalCount, limit));
+  });
 
   useEffect(() => {
     fetchPosts();
   }, []);
-
-  async function fetchPosts() {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts",
-    );
-    setPosts(response.data);
-  }
 
   const removePost = useCallback((id) => {
     setPosts((prev) => prev.filter((p) => p.id !== id));
@@ -60,12 +69,36 @@ function App() {
         filter={filter}
         setFilter={setFilter}
       />
-      <PostList
-        className="post-delete-btn"
-        remove={removePost}
-        title="Список какой-то хуйни"
-        posts={sortedAndSearchedPosts}
-      />
+      {postError && (
+        <h1 className="post__list__title">Произошла ошибка {postError}</h1>
+      )}
+      {isPostsLoading ? (
+        <div className="loader-wrapper">
+          <Loader />
+        </div>
+      ) : (
+        <PostList
+          className="post-delete-btn"
+          remove={removePost}
+          title="Список какой-то хуйни"
+          posts={sortedAndSearchedPosts}
+        />
+      )}
+      <div className="pagination-wrapper">
+        {pagesArray.map((p) => (
+          <MyButton
+            onClick={() => setPage(p)}
+            key={p}
+            className={
+              page === p
+                ? "pagination__item pagination__item--active"
+                : "pagination__item"
+            }
+          >
+            {p}
+          </MyButton>
+        ))}
+      </div>
     </div>
   );
 }
